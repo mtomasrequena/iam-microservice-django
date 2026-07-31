15677a387b0f118b2558c83b91ea3d69df966f54
Aldito <69595290+mtomasrequena@users.noreply.github.com>
2026-07-31 13:32:01 -0300

    Revert "feat: implement Custom User Model and Admin Backoffice"

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import CustomUser
 
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'role', 'is_staff', 'is_kyc_verified')
-    search_fields = ('email', 'first_name', 'last_name')
-    list_filter = ('role', 'is_staff', 'is_kyc_verified')
+# Register your models here.
