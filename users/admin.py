9b3b48e487971dc82093219cc520dd0a709e2752
mtomasrequena <m.requenachaves@gmail.com>
2026-07-31 13:24:31 -0300

    feat: configure django admin interface for custom user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'role', 'is_staff', 'is_kyc_verified')
+    search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ('role', 'is_staff', 'is_kyc_verified')
