0079482baecfe15ea8ab3f1cfa2ba77c4a7d339a
mtomasrequena <m.requenachaves@gmail.com>
2026-08-02 14:50:55 -0300

    feat: implement Custom User Model and Admin Backoffice

--- users/models.py
+++ users/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    class RoleChoices(models.TextChoices):
+        ADMIN = 'ADMIN', _('Admin')
+        CLIENT = 'CLIENT', _('Client')
+        AUDITOR = 'AUDITOR', _('Auditor')
+
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(_('first name'), max_length=150, blank=True)
+    last_name = models.CharField(_('last name'), max_length=150, blank=True)
+    
+    phone_number = models.CharField(_('phone number'), max_length=20, blank=True, null=True)
+    role = models.CharField(_('role'), max_length=20, choices=RoleChoices.choices, default=RoleChoices.CLIENT)
+    is_kyc_verified = models.BooleanField(_('KYC verified'), default=False)
+    
+    is_staff = models.BooleanField(_('staff status'), default=False)
+    is_active = models.BooleanField(_('active'), default=True)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('last update'), auto_now=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [] 
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def __str__(self):
+        return self.email
