19f55cddc7d8783938317573e0257055e7a6d60d
mtomasrequena <m.requenachaves@gmail.com>
2026-08-11 10:05:02 -0300

    fix(users): import missing APIView class in views to resolve NameError

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from django.contrib.auth import get_user_model
 from .serializers import UserRegistrationSerializer
